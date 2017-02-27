d4ce46372b248ba2c6107311cd7cd72e206697fe
AllenZeng <zengjuncm@gmail.com>
2017-02-27 17:46:31 +0800

    add level 2

--- js-bind/bind.js
+++ js-bind/bind.js
@@ -9,3 +9,15 @@ Function.prototype.bind = function (content) {
 	}
 }
 
+// 2
+// 增加浏览器功能嗅探
+Function.prototype.bind = Function.prototype.bind || function (content) {
+	// this 为调用bind方法的实际函数
+	var me = this;
+	var argsArray = Array.prototype.slice.call(arguments);
+
+	return function () {
+		return me.apply(content, argsArray.slice(1));
+	}
+}
+
