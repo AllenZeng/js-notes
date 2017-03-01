819ca6540b2d0c64c1add1a6c7429ecbafd50a3f
AllenZeng <zengjuncm@gmail.com>
2017-03-01 16:49:25 +0800

    Add es5-shim 实现

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # js-notes
+
+
+## bind的实现方式[参考链接](https://zhuanlan.zhihu.com/p/25483361)
--- js-bind/bind.js
+++ js-bind/bind.js
@@ -56,3 +56,53 @@ Function.prototype.bind = Function.prototype.bind || function (context) {
   bound.prototype = new fNOP(); // 利用继承，继承调用函数的prototype
   return bound;
 }
+
+
+// ES5-shim
+
+function isCallable(value) { 
+  if (typeof value !== 'function') { 
+    return false; 
+  }
+}
+
+var array_concat = Array.prototype.concat;
+var array_slice = Array.prototype.slice;
+var array_push = Array.prototype.push;
+
+Function.prototype.bind = Function.prototype.bind || function (that) {
+  var target = this;
+  if (!isCallable(target)) {
+    throw new TypeError('Function.prototype.bind called on incompatible ' + target);
+  }
+
+  var args = array_slice.call(arguments, 1);
+  var bound;
+
+  var binder = function () {
+    if (this instanceof bound) {
+      var result = target.apply(this, array_concat.call(args, array_slice.call(arguments)));
+      if ($Object(result) === result) {
+        return result;
+      }
+      return this;
+    } else {
+      return target.apply(that, array_concat.call(args, array_slice.call(arguments)));
+    }
+  };
+
+  var boundLength = Math.max(0, target.length - args.length);
+  var boundArgs = [];
+  for (var i = 0; i < boundLength; i++) {
+    array_push.call(boundArgs, '$' + i);
+  }
+
+  bound = Function('binder', 'return function (' + boundArgs.join(',') + '){ return binder.apply(this, arguments); }')(binder);
+
+  if (target.prototype) {
+    Empty.prototype = target.prototype;
+    bound.prototype = new Empty();
+    Empty.prototype = null;
+  }
+  return bound;
+}
